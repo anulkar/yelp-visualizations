c07a079b73c728d3fa4adf0a9df9c009bfb4b881
Atul Nulkar <nulkar@gmail.com>
2020-04-13 23:09:33 -0400

    Code to load Yelp business json dataset to MongoDB

---
+++ load_yelp_dataset.py
@@ -0,0 +1,28 @@
+import json
+import os
+import pymongo
+
+# Setup connection to mongodb
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# Select database and collection to use
+db = client.yelp_dataset
+businesses = db.businesses
+
+# Path to Yelp's Business JSON Dataset file
+yelp_business_dataset_path = "assets/data/yelp_converted_business_dataset_records.json"
+
+# Load the Business JSON file
+with open(yelp_business_dataset_path) as json_file:
+    yelp_business_json = json.load(json_file)
+
+# Delete all existing business documents/rows from the DB
+print("Deleting all existing Yelp Business data from Mongo...Hang Tight!")
+deleted_businesses = db.businesses.delete_many({})
+print(f"Deleted {deleted_businesses.deleted_count} documents from Mongo!")
+
+# Insert a fresh set of business documents/rows to the DB
+print("Loading a fresh Yelp Business Dataset to Mongo...Hang Tight!")
+new_businesses = db.businesses.insert_many(yelp_business_json)
+print(f"All Yelp Business Data has been Uploaded: {db.businesses.count_documents({})}")

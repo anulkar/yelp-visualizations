b39dbb80926ebbd756f16b50553fefccdc0de9c0
Atul Nulkar <nulkar@gmail.com>
2020-04-20 23:46:18 -0400

    Testing home route to pull only Toronto business data

--- yelp_app.py
+++ yelp_app.py
@@ -1,4 +1,4 @@
-# import necessary libraries
+# Import necessary libraries
 from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
@@ -6,7 +6,7 @@ import json
 from bson import json_util
 from bson.json_util import dumps
 
-# create instance of Flask app
+# Create instance of Flask app
 app = Flask(__name__)
 CORS(app)
 
@@ -19,17 +19,15 @@ mongo = PyMongo(app)
 def home():
 
     # Find data
-    businesses = mongo.db.businesses.find(limit=10)
+    toronto_businesses = mongo.db.toronto_businesses.find(limit=10)
 
     yelp_business_data = []
     
     # return yelp_business_data
-    for business in businesses:
+    for business in toronto_businesses:
         yelp_business_data.append(business)
 
     yelp_business_data = json.dumps(yelp_business_data, default=json_util.default)
-    print(yelp_business_data)
-    # return render_template("index.html"), yelp_business_data=yelp_business_data)
     return yelp_business_data
 
 if __name__ == "__main__":

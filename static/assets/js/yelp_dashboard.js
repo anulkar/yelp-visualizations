c3f57e65485c27665ede38ac8563ec906ef681e8
Atul Nulkar <nulkar@gmail.com>
2020-04-20 23:47:15 -0400

    Renamed yelp_app.js to yelp_dashboard.js

--- static/assets/js/yelp_dashboard.js
+++ static/assets/js/yelp_dashboard.js
@@ -3,12 +3,9 @@ yelpBizData = "http://127.0.0.1:5000/";
 
 //Read the JSON dataset using d3
 d3.json(yelpBizData).then(yelpData => {
+    console.log(yelpData)
     populateData(yelpData);
 });
-// fetch('http://127.0.0.1:5000/')
-//   //.then(res => res.json()) // comment this out for now
-//   .then(res => res.text())          // convert to plain text
-//   .then(text => console.log(text))  // then log it out
 
 function populateData(yelpData) {
     // Get a reference to the dropdown list

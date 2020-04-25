6ad390a085cfd8e10ebb5bb3122a9963e2f52018
Stone Powell <spowell11@gmail.com>
2020-04-25 09:41:59 -0400

    uploading stones files

---
+++ static/assets/js/stones_visualization.js
@@ -0,0 +1,30 @@
+yelpBizData = "http://127.0.0.1:5000/";
+
+d3.json(yelpBizData).then(function(yelpdata) {
+    //console.log(yelpdata);
+    var ydata = yelpdata
+    var reviewcounts = ydata.map(row => row.review_count)
+   // console.log(reviewcounts)
+    var yelpstars= ydata.map(row=> row.stars)
+    //console.log(yelpstars)
+
+// Create Dataset using anychary.js Library
+  
+  var dataset = anychart.data.set(ydata);
+  //console.log(dataset);
+  // Map data
+  var mapping = dataset.mapAs({x:"stars", value:"review_count"});
+
+  // create the chart
+  var chart = anychart.scatter(mapping);
+
+
+  // set the container
+
+  chart.container("average-stars");
+
+  // Intiate drawing the chart
+
+  chart.draw();
+
+});

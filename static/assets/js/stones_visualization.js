c7528732c4aff04d973acd499c6c136dfafb2e63
Stone Powell <spowell11@gmail.com>
2020-04-25 14:04:57 -0400

    adding tagCloud

--- static/assets/js/stones_visualization.js
+++ static/assets/js/stones_visualization.js
@@ -27,3 +27,51 @@ d3.json(yelpBizData).then(function(yelpdata) {
   chart.draw();
 
 });
+
+d3.json(yelpBizData).then(function(tipsdata) {
+  //console.log(tipsdata);
+  var tdata = tipsdata
+ 
+  // Map the tips in the database
+  var tips = tdata.map(row => row.text)
+
+  // Join the tips to create one string
+  tips = tips.join(" ")
+
+// console.log(tips);
+
+// create the chart using anychary.js Library
+var chart = anychart.tagCloud();
+
+// Set up criteria for tagCloud
+chart.data(tips, {
+  mode: "by-word",
+  maxItems: 800,
+  ignoreItems: [
+                "the",
+                "and",
+                "he",
+                "or",
+                "of",
+                "in",
+                "a",
+                "to",
+                "is",
+                "for",
+               ]
+  });
+
+// Set up the tooltip to display the % of words in the sample
+chart.tooltip().format("{%yPercentOfTotal}% ({%value})\n\n{%custom_field}");
+
+
+// set the container
+
+chart.container("tips-cloud");
+
+// Intiate drawing the chart
+
+chart.draw();
+
+
+});

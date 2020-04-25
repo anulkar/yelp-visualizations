bfc97983ed9580eb9df88e1003098172f526b168
Keith Horbin <keith.horbin.ds@gmail.com>
2020-04-25 11:08:23 -0400

    gauge push

--- Keith/bubble test.js
+++ Keith/bubble test.js
@@ -1,18 +1,31 @@
-anychart.onDocumentReady(function() {
-
-    // load data
-    anychart.data.loadJsonFile('yelp_toronto_business_dataset_records.json', function (data) {
+function buildCharts(sample) {
+    d3.json("yelp_toronto_business_dataset_records.json").then((data) => {
+      var samples = data.samples;
+      var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
   
-      // create a chart
-      chart = anychart.bubble(data);
-      // set a chart title
-      chart.title("Top 30 Grossing Movies");
-      // set titles for axes
-      chart.review_count().title("Reviews");
-      chart.stars().title("Rating");
-      // draw the chart
-      chart.container("container").draw();
+      var otu_ids = result.otu_ids;
+      var otu_labels = result.otu_labels;
+      var sample_values = result.sample_values;
   
-    })
-  
-  });
+      // Build a Bubble Chart
+      var bubbleLayout = {
+        title: "Bacteria Cultures Per Sample",
+        margin: { t: 0 },
+        hovermode: "closest",
+        xaxis: { title: "OTU ID" },
+        margin: { t: 30}
+      };
+      var bubbleData = [
+        {
+          x: otu_ids,
+          y: sample_values,
+          text: otu_labels,
+          mode: "markers",
+          marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: "Earth"
+          }
+        }
+      ];
